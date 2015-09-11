6a5407847908b0531b2dc4d62e2ce4b7b635fd47
tkutsarov <tkutsarov@yahoo.com>
2015-09-11 19:01:51 +0300

    removed unused code for changing progress styles

--- public_html/script/main.js
+++ public_html/script/main.js
@@ -46,16 +46,6 @@ function showSubmenu() {
 }
 
 $(document).ready(function () {
-
-    $('div.finished-course div.course-panel div.course-info div.bottom-info div.left-side-info \n\
-           div.progress-panel div.progress div.progress-bar').css('width', '100%');
-    $('div.finished-course-submenu div.course-panel div.course-info div.bottom-info div.left-side-info \n\
-           div.progress-panel div.progress div.progress-bar').css('width', '100%');
-    $('div.finished-course div.course-panel div.course-info div.bottom-info div.left-side-info \n\
-           div.progress-panel div.progress-rate').text("100% finished");
-    $('div.finished-course-submenu div.course-panel div.course-info div.bottom-info div.left-side-info \n\
-           div.progress-panel div.progress-rate').text("100% finished");
-
     showSubmenu();
     limitTitleText();
     dynamicResizeContainer();
